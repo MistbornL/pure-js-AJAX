bc1c02d67017a26c9a3d77ed0c35eb7a4b9a7aa9
MistbornL <lashaloria3@gmail.com>
2022-09-17 15:34:38 +0400

    basic request complete

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 
 const button = document.getElementById("button");
 button.addEventListener("click", loadText);
+text = document.getElementById("text");
 
 function loadText() {
   const xhr = new XMLHttpRequest();
 
   xhr.onload = function () {
     if (this.status === 200) {
-      console.log(this.responseText);
+      text.innerHTML = this.responseText;
+    } else if (this.status === 404) {
+      text.innerHTML = "NOT FOUND";
     }
   };
 
-  xhr.open("GET", "sample.txt", true);
+  //   xhr.onreadystatechange = function () {
+  //     if (this.readyState === 4 && this.status === 200) {
+  //       console.log(this.responseText);
+  //     }
+  //   };
+
+  xhr.onerror = function () {
+    alert("error");
+  };
+
+  xhr.open("GET", "sample2.txt", true);
   xhr.send();
 }
